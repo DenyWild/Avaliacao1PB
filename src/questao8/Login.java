d5bfb519f4141b6cbd84ea0dcb95f59988116013
DenyWild <denyswildson2011@gmail.com>
2022-02-06 10:23:36 -0300

    Commitando função validacaoLogin

---
+++ src/questao8/Login.java
@@ -0,0 +1,67 @@
+package questao8;
+
+import java.lang.Integer;
+import java.util.Calendar;
+
+public class Login {
+
+	private String usuario;
+	private String senha;
+
+	public Login(String usuario, String senha) {
+		super();
+		this.usuario = usuario;
+		this.senha = senha;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public static void validacaoLogin(String aux1, String aux2, Login[] comp, int hora) {
+
+		for (int i = 0; i <= comp.length; i++) {
+
+			if (i == 3) {
+				System.out.println("Usuário e/ou senha incorretos.");
+				i--;
+				break;
+			}
+			if (aux1.equalsIgnoreCase(comp[i].getUsuario()) && aux2.equalsIgnoreCase(comp[i].getSenha())) {
+				horaDoDia(hora);
+				break;
+			}
+
+		}
+	}
+
+	public static void horaDoDia(int hora) {
+
+		if (hora >= 6 && hora < 12) {
+			System.out.println("Bom dia, você se logou ao nosso sistema.");
+		}
+		if (hora >= 12 && hora < 18) {
+			System.out.println("Boa tarde, você se logou ao nosso sistema.");
+		}
+		if (hora >= 18 && hora < 0) {
+			System.out.println("Boa noite, você se logou ao nosso sistema.");
+		}
+		if (hora >= 0 && hora < 6) {
+			System.out.println("Boa Madrugada, você se logou ao nosso sistema.");
+		}
+
+	}
+
+}
--- src/questao8/questao8Main.java
+++ src/questao8/questao8Main.java
@@ -12,56 +12,24 @@ public class questao8Main {
 		Calendar cal = Calendar.getInstance();
 		int hora = cal.get(Calendar.HOUR_OF_DAY);
 
-		questao8Complemento[] comp = new questao8Complemento[3];
+		Login[] login = new Login[3];
 
 //VARIAVEIS PARA RECEBER O USUARIO E A SENHA
-		String aux1;
-		String aux2;
+		String loginUsuario;
+		String loginSenha;
 //ARRAYS CONTENDO O CADASTRO
-		comp[0] = new questao8Complemento("joana", "1234");
-		comp[1] = new questao8Complemento("compasso", "estagio");
-		comp[2] = new questao8Complemento("denys", "4321");
+		login[0] = new Login("joana", "1234");
+		login[1] = new Login("compasso", "estagio");
+		login[2] = new Login("denys", "4321");
 
 		System.out.println("Por favor nos informe seu usuario e senha para login");
 		System.out.println("Usuario: ");
-		aux1 = input.nextLine();
+		loginUsuario = input.nextLine();
 		System.out.println("Senha: ");
-		aux2 = input.nextLine();
-		validacaoLogin(aux1, aux2, comp, hora);
+		loginSenha = input.nextLine();
+
+		Login.validacaoLogin(loginUsuario, loginSenha, login, hora);
 
 	}
 
-	public static void validacaoLogin(String aux1, String aux2, questao8Complemento[] comp, int hora) {
-
-		for (int i = 0; i <= comp.length; i++) {
-
-			if (i == 3) {
-				System.out.println("Usuário e/ou senha incorretos.");
-				i--;
-				break;
-			}
-			if (aux1.equalsIgnoreCase(comp[i].getUsuario()) && aux2.equalsIgnoreCase(comp[i].getSenha())) {
-				horaDoDia(hora);
-				break;
-			}
-
-		}
-	}
-
-	public static void horaDoDia(int hora) {
-
-		if (hora >= 6 && hora < 12) {
-			System.out.println("Bom dia, você se logou ao nosso sistema.");
-		}
-		if (hora >= 12 && hora < 18) {
-			System.out.println("Boa tarde, você se logou ao nosso sistema.");
-		}
-		if (hora >= 18 && hora < 0) {
-			System.out.println("Boa noite, você se logou ao nosso sistema.");
-		}
-		if (hora >= 0 && hora < 6) {
-			System.out.println("Boa Madrugada, você se logou ao nosso sistema.");
-		}
-
-	}
 }
